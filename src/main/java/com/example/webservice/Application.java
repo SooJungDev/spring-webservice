724b1a88ddf1225129f14b4d88bc68c6562c2241
crystal <crystal2840@gmail.com>
2018-11-27 15:02:31 +0900

    spring batch 추가

--- src/main/java/com/example/webservice/Application.java
+++ src/main/java/com/example/webservice/Application.java
@@ -1,9 +1,11 @@
 package com.example.webservice;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableBatchProcessing // 배치 기능 활성화
 @EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
